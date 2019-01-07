60d727297e435cdd8283be4dc60e686a19f4610d
Tal Levy <orntal17@gmail.com>
2019-01-07 17:06:11 +0200

    MySerialServer is now up and running

--- MySerialServer.h
+++ MySerialServer.h
@@ -2,11 +2,19 @@
 #define SERVER_CLIENT_PROJECT_MYSERIALSERVER_H
 
 #include "server_side.h"
+#include "vector"
+#include <netdb.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include "strings.h"
 
 using namespace server_side;
 
 class MySerialServer : public Server {
     int sockfd;
+    int newsockfd;
+    bool isConnected;
 
 public:
 
